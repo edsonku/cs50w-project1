6af8d37c224ed288e7b38a17e841645e0fcdcf14
estefanyrizo <estefanyrizo@icloud.com>
2021-08-25 13:25:33 -0600

    biblioteca  agragada

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 from flask import Flask, session
 from flask_session import Session
@@ -17,9 +18,22 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine("postgresql://kzpvqapggnigwp:fb9ae8eeee9d58ac9fa2ec10ad9f9ff7587a0505dc26de048128c2aee5de47fa@ec2-54-159-35-35.compute-1.amazonaws.com:5432/dfk5gp8sokvmov")
+engine = create_engine(
+    "postgresql://kzpvqapggnigwp:fb9ae8eeee9d58ac9fa2ec10ad9f9ff7587a0505dc26de048128c2aee5de47fa@ec2-54-159-35-35.compute-1.amazonaws.com:5432/dfk5gp8sokvmov")
 db = scoped_session(sessionmaker(bind=engine))
-library = open("books.csv")
 
-for isbn, title, author, year in library:
-    print(f"{isbn} to {title} by {author} in {year}")
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO library (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                   {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(
+            f"Añadido a la biblioteca {title} de {author} escrito en el año {year}")
+
+    db.commit()
+
+
+if __name__ == "__main__":
+    main()
